b46cffe09376cb787661db9fe2d3d212685bcfe7
JhonSmit <odooenterprisetest@gmail.com>
2016-12-29 12:22:05 -0500

    shipping address

--- server/ecore/extend/account_invoice_shipping_address/sale.py
+++ server/ecore/extend/account_invoice_shipping_address/sale.py
@@ -7,7 +7,7 @@ from ecore import api, fields, models
 class sale_order(orm.Model):
     _inherit = 'sale.order'
 
-    def _prepare_invoice(self, cr, uid, order, lines, context=None):
-        res = super(sale_order, self)._prepare_invoice(cr, uid, order=order, Lines=Lines, context=context)
-        res.update({'address_shipping_id': order.partner_shipping_id.id, })
+    def _prepare_invoice(self):
+        res = super(sale_order, self)._prepare_invoice()
+        res.update({'address_shipping_id': order.partner_shipping_id.id,})
         return res
